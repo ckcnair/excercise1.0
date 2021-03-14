aefb2725bc4d72eeebbc813699ae7f06f940903c
ckcnair <ckcnair@gmail.com>
2021-03-15 04:22:00 +0530

    timestamp

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,10 +1,14 @@
 import * as actions from "./actionTypes";
 
 export const createNote = (note) => {
+  let today = new Date();
+  let date = `${today.getDate()}-${today.getMonth()}-${today.getFullYear()} ${today.getHours()}:${today.getMinutes()}`;
+
   return {
     type: actions.NOTE_CREATED,
     payload: {
       note,
+      date,
     },
   };
 };
