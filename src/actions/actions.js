39527dbdb2bd3b3ee3b25f2b8924d1add7cff2e0
ckcnair <ckcnair@gmail.com>
2021-03-15 03:06:57 +0530

    added payload to action object

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -3,13 +3,17 @@ import * as actions from "./actionTypes";
 export const createNote = (note) => {
   return {
     type: actions.NOTE_CREATED,
-    note,
+    payload: {
+      note,
+    },
   };
 };
 
 export const removeNote = (id) => {
   return {
     type: actions.NOTE_REMOVED,
-    id: id,
+    payload: {
+      id,
+    },
   };
 };
